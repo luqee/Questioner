64f6be9f1deb7f451cd7bdcf68fb6cf8914f03bb
Luke Nzang'u <saintlukke@gmail.com>
2019-01-09 21:24:57 +0300

    [Finishes #163001713] Style meetup deletion dialog.

---
+++ UI/static/js/index.js
@@ -0,0 +1,21 @@
+// Get the modal
+var modal = document.getElementById('deleteModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+var showModal = function(){
+    modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
